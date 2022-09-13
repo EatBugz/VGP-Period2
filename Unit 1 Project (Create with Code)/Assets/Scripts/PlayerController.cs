163c28317bae1b94f480e118f4d24b43810670b3
EatBugz <77813942+EatBugz@users.noreply.github.com>
2022-09-13 09:32:49 -0700

    Unit 1 Project Done
    Finished the project for lesson 1 with Create with Code

---
+++ Unit 1 Project (Create with Code)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 20f;
+    public float turnSpeed = 45f;
+    private float hI;
+    private float vI;
+
+    // move thing forward
+    void Update()
+    {
+        hI = Input.GetAxisRaw("Horizontal");
+        vI = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vI);
+        transform.Rotate(Vector3.up, turnSpeed * hI * Time.deltaTime);
+    }
+}
