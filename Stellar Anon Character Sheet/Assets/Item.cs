6e990495455462517d17525dd0f412654405e600
EatBugz <77813942+EatBugz@users.noreply.github.com>
2022-12-17 15:40:22 -0800

    a;sldkfja

---
+++ Stellar Anon Character Sheet/Assets/Item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    private int amount;
+
+    public Item() { }
+
+
+    /// Accessors \\\
+    public int getAmount() { return this.amount; }
+}
