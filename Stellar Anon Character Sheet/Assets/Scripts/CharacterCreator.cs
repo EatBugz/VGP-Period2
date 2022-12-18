e54fa11b934304dab6a1d6d575dbe7f1d1ca5681
EatBugz <77813942+EatBugz@users.noreply.github.com>
2022-12-17 19:21:28 -0800

    assault dropdown
    ( :

--- Stellar Anon Character Sheet/Assets/Scripts/CharacterCreator.cs
+++ Stellar Anon Character Sheet/Assets/Scripts/CharacterCreator.cs
@@ -13,16 +13,20 @@ public class CharacterCreator : MonoBehaviour
     private TMP_InputField nameInputField;
     public GameObject speciesInput;
     private TMP_Dropdown speciesInputField;
+    public GameObject classInput;
+    private TMP_Dropdown classInputField;
 
     // user input saves
     [Header("User Input")]
     public string charName;
     public Character.SpeciesType charSpecies;
+    public Character.ClassType charClass;
 
     // get input fields
     void Start() {
         nameInputField = nameInput.GetComponent<TMP_InputField>();
         speciesInputField = speciesInput.GetComponent<TMP_Dropdown>();
+        classInputField = classInput.GetComponent<TMP_Dropdown>();
     }
 
     // check which input field is selected so if the user presses enter
@@ -48,5 +52,13 @@ public class CharacterCreator : MonoBehaviour
         else if (speciesInputField.options[speciesInputField.value].text == "Ilid") charSpecies = Character.SpeciesType.Ilid;
         else if (speciesInputField.options[speciesInputField.value].text == "Numo") charSpecies = Character.SpeciesType.Numo;
         else if (speciesInputField.options[speciesInputField.value].text == "Stellos") charSpecies = Character.SpeciesType.Stellos;
+    }
+
+    // method that gets the class from the dropdown
+    public void getClass() {
+        if (classInputField.options[classInputField.value].text == "Assault") charClass = Character.ClassType.Assault;
+        else if (classInputField.options[classInputField.value].text == "Scout") charClass = Character.ClassType.Scout;
+        else if (classInputField.options[classInputField.value].text == "Support") charClass = Character.ClassType.Support;
+        else if (classInputField.options[classInputField.value].text == "Medic") charClass = Character.ClassType.Medic;
     }
 }
