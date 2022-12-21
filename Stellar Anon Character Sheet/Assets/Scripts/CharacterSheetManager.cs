087ede613644e90c44ab9769a8f3d964738452de
EatBugz <77813942+EatBugz@users.noreply.github.com>
2022-12-21 08:25:34 -0800

    h

--- Stellar Anon Character Sheet/Assets/Scripts/CharacterSheetManager.cs
+++ Stellar Anon Character Sheet/Assets/Scripts/CharacterSheetManager.cs
@@ -9,7 +9,23 @@ public class CharacterSheetManager : MonoBehaviour
     private Character selectedCharacter;
     public int selectedCharacterIndex = -1;
 
+    [Header("Main Info Texts")]
     public TextMeshProUGUI nameTitleText;
+    public TextMeshProUGUI movementText;
+    public TextMeshProUGUI hitDieTypeText;
+    public TextMeshProUGUI maxHitPointsText;
+    public TextMeshProUGUI generalSkillText;
+    public TextMeshProUGUI meleeSkillText;
+    public TextMeshProUGUI firearmSkillText;
+    public TextMeshProUGUI intelligenceSkillText;
+    public TextMeshProUGUI dexteritySkillText;
+    public TextMeshProUGUI generalDeterminationText;
+    public TextMeshProUGUI determinationOfWillText;
+    public TextMeshProUGUI determinationOfBodyText;
+    public TextMeshProUGUI generalPowerText;
+    public TextMeshProUGUI powerOfStrengthText;
+    public TextMeshProUGUI powerOfCharismaText;
+    public TextMeshProUGUI saveText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +36,31 @@ public class CharacterSheetManager : MonoBehaviour
 
     // update visuals
     public void updateSheet() {
+        // update name
         nameTitleText.text = ""+selectedCharacter.getName()+" | "+selectedCharacter.getSpeciesString()+" | "+selectedCharacter.getProfessionString()+" "+selectedCharacter.getLevel();
+
+        // show visuals
+        movementText.text = ""+selectedCharacter.getMovement();
+        hitDieTypeText.text = "d" + selectedCharacter.getHitDieType();
+        maxHitPointsText.text = ""+selectedCharacter.getMaxHitPoints();
+        generalSkillText.text = ""+selectedCharacter.getGeneralSkill();
+        meleeSkillText.text = ""+selectedCharacter.getMeleeSkill();
+        firearmSkillText.text = ""+selectedCharacter.getFirearmSkill();
+        intelligenceSkillText.text = ""+selectedCharacter.getIntelligenceSkill();
+        dexteritySkillText.text = ""+selectedCharacter.getDexteritySkill();
+        generalDeterminationText.text = ""+selectedCharacter.getGeneralDetermination();
+        determinationOfWillText.text = ""+selectedCharacter.getDeterminationOfWill();
+        determinationOfBodyText.text = ""+selectedCharacter.getDeterminationOfBody();
+        generalPowerText.text = ""+selectedCharacter.getGeneralPower();
+        powerOfStrengthText.text = ""+selectedCharacter.getPowerOfStrength();
+        powerOfCharismaText.text = ""+selectedCharacter.getPowerOfCharisma();
+        saveText.text = ""+selectedCharacter.getSave();
+
+        /* For later
+        startingFeatureText.text = "Starting Feature: " + createdCharacter.getStartingFeature();
+        weaponProfText.text = "Weapon Proficiencies: " + createdCharacter.getWeaponProfString();
+        armorProfText.text = "Armor Proficiencies: " + createdCharacter.getArmorProfString();
+        */
     }
 
     // methods that save an load the selected character
